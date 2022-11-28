3d75f976915653810fd271785cde9518770ba89b
aleksandra <ochirova.alex@gmail.com>
2022-11-28 20:25:30 +0400

    remove skipping linter

--- js/card.js
+++ js/card.js
@@ -28,18 +28,14 @@ const featuresDict = {
 const createPopupEl = ({author, offer}) => {
   const cardElement = cardTemplate.cloneNode(true);
   for (const key in offer) {
-    //нужно чтоб проверяло на undefined и null (не offer[key] !== null, а offer[key] != null) но линтеру так не нравится
-    // eslint-disable-next-line eqeqeq
-    if (offer[key] != null) {
+    if (offer[key] !== null) {
       if (key === 'features') {
         for (const feature in featuresDict) {
           if (!offer.features.includes(feature)) {
             cardElement.querySelector(featuresDict[feature]).classList.add('hidden');
           }
         }
-        //тут тоже самое
-      // eslint-disable-next-line eqeqeq
-      } else if (key === 'rooms' && offer.guests != null) {
+      } else if (key === 'rooms' && offer.guests !== null) {
         cardElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
       } else if (key === 'checkin' && offer.checkout) {
         cardElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
